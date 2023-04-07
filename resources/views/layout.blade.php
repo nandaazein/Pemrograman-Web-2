ee32ab691426cc637b5858a79b171c9a265c06bf
nandaazein <difanazein@gmail.com>
2023-04-07 20:35:26 +0800

    perbaiki login

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,7 +21,20 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a class="navbar-brand" href="#">Skincare Pricing</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="\sesi\logout">Logout</a>
+            </li>
 
+                   </div>
+      </nav>
+   
     <div class="container py-12" >
         <div class="d-flex justify-content-center">
             <div class="col-lg-12">
